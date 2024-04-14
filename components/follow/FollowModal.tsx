b20f9044625e3a0a08188d778b8a4e93a2a3cd18
Yukine213 <kondratenkov1109@gmail.com>
2024-04-14 18:40:35 +0300

    make modal responsive and fix the bug where you couldn't scroll after closing the modal

--- components/follow/FollowModal.tsx
+++ components/follow/FollowModal.tsx
@@ -25,41 +25,59 @@ const FollowModal = ({
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        setIsOpenFollowers(false);
-        setIsOpenFollowing(false);
+    const handleScroll = (event: WheelEvent) => {
+      if (ref.current && ref.current.contains(event.target as Node)) {
+        event.stopPropagation();
       }
     };
 
-    const body = document.querySelector("body");
-
     if (isOpenFollowers || isOpenFollowing) {
-      body?.classList.add("overflow-hidden");
+      document.body.style.overflow = "hidden";
+      document.addEventListener("wheel", handleScroll, { passive: false });
     } else {
-      body?.classList.remove("overflow-hidden");
+      document.body.style.overflow = "auto";
+      document.removeEventListener("wheel", handleScroll);
     }
 
+    // Cleanup function to remove overflow class when unmounting or modal is closed
+    return () => {
+      document.body.style.overflow = "auto"; // Reset overflow style
+      document.removeEventListener("wheel", handleScroll);
+    };
+  }, [isOpenFollowers, isOpenFollowing]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsOpenFollowers(false);
+      setIsOpenFollowing(false);
+    }
+  };
+
+  useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [
-    isOpenFollowers,
-    setIsOpenFollowers,
-    isOpenFollowing,
-    setIsOpenFollowing,
-  ]);
+  }, [handleClickOutside]);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handleClickOutside]);
 
   return (
     <div
       id="modal"
-      className="fixed  top-0 left-0 z-50 w-full h-full bg-gray-800 bg-opacity-80 flex justify-center items-center"
+      className="fixed  top-0 left-0 z-50 w-full h-full bg-gray-800 bg-opacity-80 flex justify-center items-center "
     >
       <div
         ref={ref}
-        className="bg-black shadow-sm w-[500px] max-h-[500px] overflow-y-auto rounded-lg p-6"
+        className="bg-black shadow-sm w-[500px] max-h-[500px] overflow-y-auto rounded-lg mx-2 md:mx-0 p-6"
       >
         {follow.map((follower) => (
           <div key={follower.userId} className="flex  gap-2 items-center mb-4">
