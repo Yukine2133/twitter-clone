90ddb867ae8e9e61f9a5df3aac0c2ec67b2a8c83
Yukine213 <kondratenkov1109@gmail.com>
2024-04-15 20:28:17 +0300

    add close button

--- components/tweets/AddTweet.tsx
+++ components/tweets/AddTweet.tsx
@@ -92,6 +92,12 @@ const AddTweet = ({ user }: { user: KindeUser }) => {
       {isOpen && (
         <div className="fixed  top-0 left-0 z-50 w-full h-full bg-gray-800 bg-opacity-80 flex justify-center items-center">
           <div className="bg-black shadow-sm w-[700px] rounded-lg mx-2 md:mx-0 p-5 md:p-8">
+            <h6
+              className="cursor-pointer text-slate-500 flex justify-end"
+              onClick={() => setIsOpen(false)}
+            >
+              X
+            </h6>
             <UploadDropzone
               endpoint={"media"}
               onClientUploadComplete={(res) => {
