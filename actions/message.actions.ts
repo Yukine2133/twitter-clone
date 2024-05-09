9256ff6da825c9ca926a1db10a574dcd52a188d9
Yukine213 <kondratenkov1109@gmail.com>
2024-05-09 21:16:08 +0300

    add getMessages server action

--- actions/message.actions.ts
+++ actions/message.actions.ts
@@ -4,6 +4,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Message } from "@/models/message.model";
 import { connectDb } from "@/utils/connectDb";
 import { fetchUser } from "./user.actions";
+import { revalidatePath } from "next/cache";
 
 export const sendMessage = async (recipientId: string, formData: FormData) => {
   try {
@@ -24,7 +25,21 @@ export const sendMessage = async (recipientId: string, formData: FormData) => {
     });
 
     await message.save();
+    revalidatePath("/messages");
   } catch (error) {
     throw new Error(`Failed to send message: ${error}`);
+  }
+};
+
+export const getMessages = async (userId: string) => {
+  try {
+    // Retrieve messages where the user is either the sender or the recipient
+    const messages = await Message.find({
+      $or: [{ sender: userId }, { recipient: userId }],
+    }).sort({ createdAt: 1 }); // Sort messages by createdAt timestamp in ascending order
+    return messages;
+  } catch (error) {
+    console.error("Error fetching messages:", error);
+    throw new Error("Failed to fetch messages");
   }
 };
