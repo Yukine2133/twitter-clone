71a2cd09815349c315229567462603bb25739ad2
Yukine213 <kondratenkov1109@gmail.com>
2024-07-02 16:51:04 +0300

    fix build error in bookmarks page

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -5,16 +5,33 @@ import Link from "next/link";
 import React from "react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { fetchUser } from "@/actions/user.actions";
-import {
-  getColorFromHash,
-  hashString,
-} from "@/utils/differentColorForBookmarkFolders";
+
+const colors = [
+  "bg-purple-500",
+  "bg-green-500",
+  "bg-yellow-500",
+  "bg-red-500",
+  "bg-pink-500",
+  "bg-teal-500",
+  "bg-orange-500",
+  "bg-amber-500",
+  "bg-lime-500",
+  "bg-cyan-500",
+  "bg-fuchsia-500",
+  "bg-violet-500",
+  "bg-rose-500",
+  "bg-indigo-500",
+  "bg-emerald-500",
+  "bg-indigo-500",
+];
 
 const Bookmarks = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-
-  const currentUser = await fetchUser(user?.id);
+  let currentUser;
+  if (user) {
+    currentUser = await fetchUser(user.id);
+  }
   const userBookmarkFolders = await getUserBookmarkFolders(user?.id as string);
   return (
     <div>
@@ -22,7 +39,7 @@ const Bookmarks = async () => {
         <div className="">
           <h2 className="font-semibold text-xl">Bookmarks</h2>
           <span className="text-gray-500 text-sm mt-1">
-            @{currentUser.username}
+            @{currentUser?.username}
           </span>
         </div>
         <AddBookmarkFolderButton />
@@ -43,10 +60,9 @@ const Bookmarks = async () => {
           <ChevronRightIcon className="size-5" />
         </Link>
 
-        {userBookmarkFolders?.map((folder) => {
+        {userBookmarkFolders?.map((folder, index) => {
           const folderNameWithHyphens = folder.name.replace(/\s+/g, "-");
-          const hash = hashString(folder.name);
-          const colorClass = getColorFromHash(hash);
+          const colorClass = colors[index % colors.length]; // Pick color based on index
           return (
             <Link
               key={folder._id}
