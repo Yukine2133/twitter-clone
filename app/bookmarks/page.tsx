83f899b93eb68b5dd220e316984533497c749a98
Yukine213 <kondratenkov1109@gmail.com>
2024-07-02 14:47:33 +0300

    finish ui of the bookmarks page

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,3 +1,4 @@
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import { BookmarkIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import React from "react";
@@ -13,13 +14,29 @@ const Bookmarks = () => {
       <div className="mt-4">
         <Link
           href="/bookmarks/all"
-          className="flex items-center gap-4 px-4 py-2 hover:bg-gray-600/20 transition-colors duration-300 w-full "
+          className="flex items-center justify-between gap-4 px-4 py-3 hover:bg-gray-600/20 transition-colors duration-300 w-full "
         >
-          <div className="bg-blue-500 rounded-full p-[10px] relative ">
-            <BookmarkIcon className="size-6 z-10" />
-            <BookmarkIcon className="size-6 absolute top-[7px] right-1.5" />
+          <div className="flex items-center gap-4">
+            <div className="bg-blue-500 rounded-full p-[10px] relative ">
+              <BookmarkIcon className="size-6 z-10" />
+              <BookmarkIcon className="size-6 absolute top-[7px] right-1.5" />
+            </div>
+            <p>All Bookmarks</p>
           </div>
-          <p>All Bookmarks</p>
+          <ChevronRightIcon className="size-5" />
+        </Link>
+
+        <Link
+          href="/bookmarks/:nameOfTheFolder"
+          className="flex items-center justify-between gap-4 px-4 py-3 hover:bg-gray-600/20 transition-colors duration-300 w-full "
+        >
+          <div className="flex items-center gap-4">
+            <div className="bg-purple-500 rounded-full p-[10px] ">
+              <BookmarkIcon className="size-6 z-10" />
+            </div>
+            <p>User custom folder</p>
+          </div>
+          <ChevronRightIcon className="size-5" />
         </Link>
       </div>
     </div>
