b1d80b96936353803ed1aa99ffb383d00bb0cb05
Yukine213 <kondratenkov1109@gmail.com>
2024-07-11 13:28:26 +0300

    trying to fix a bug in production that doesn't show different colors on bookmarks

--- components/tweets/TweetActions.tsx
+++ components/tweets/TweetActions.tsx
@@ -16,7 +16,25 @@ import {
 import * as solid from "@heroicons/react/24/solid";
 import useTweetActions from "@/utils/lib/hooks/useTweetActions";
 import { addBookmarkToFolder } from "@/actions/bookmark.actions";
-import { colors } from "@/utils/colors";
+
+const colors = [
+  "bg-purple-500",
+  "bg-green-500",
+  "bg-yellow-500",
+  "bg-red-500",
+  "bg-pink-500",
+  "bg-teal-500",
+  "bg-orange-500",
+  "bg-amber-500",
+  "bg-lime-500",
+  "bg-cyan-500",
+  "bg-fuchsia-500",
+  "bg-violet-500",
+  "bg-rose-500",
+  "bg-indigo-500",
+  "bg-emerald-500",
+  "bg-indigo-500",
+];
 
 interface TweetActionsProps extends ITweetProps {
   isBookmarked: boolean;
