c503adc66c68163e021dfd5bef7b608ab09c7236
Yukine213 <kondratenkov1109@gmail.com>
2025-01-19 11:10:20 +0200

    fix toggleFollow function

--- components/follow/Follow.tsx
+++ components/follow/Follow.tsx
@@ -28,23 +28,28 @@ const Follow = ({
   const [isOpenFollowers, setIsOpenFollowers] = useState(false);
   const [isOpenFollowing, setIsOpenFollowing] = useState(false);
 
-  const toggleFollowing = () => {
+  const toggleFollow = (type: string) => {
     if (userPrivate) {
       return;
     }
-    setIsOpenFollowing(!isOpenFollowing);
+
+    if (type === "following") {
+      setIsOpenFollowing(true);
+    } else {
+      setIsOpenFollowers(true);
+    }
   };
 
   return (
     <>
       <button
-        onClick={toggleFollowing}
+        onClick={() => toggleFollow("following")}
         className="text-slate-500 hover:underline decoration-white cursor-pointer"
       >
         <span className="text-gray-300">{following.length}</span> Following
       </button>
       <button
-        onClick={toggleFollowing}
+        onClick={() => toggleFollow("followers")}
         className="text-gray-500 hover:underline decoration-white cursor-pointer"
       >
         <span className="text-gray-300">{followers.length}</span> Followers
