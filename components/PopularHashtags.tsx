0591dbf77005c2e1974bf8cac2afac4de83ea4b6
Yukine213 <kondratenkov1109@gmail.com>
2025-01-19 13:09:06 +0200

    feat: improve ui of the popular hashtags

--- components/PopularHashtags.tsx
+++ components/PopularHashtags.tsx
@@ -1,17 +1,28 @@
 import { fetchPopularHashtags } from "@/actions/tweet.actions";
+import Link from "next/link";
 import React from "react";
 
 const PopularHashtags = async () => {
   const hashtags = await fetchPopularHashtags();
 
   return (
-    <div className="mt-4 w-[100px]">
-      {hashtags.map((hashtag) => (
-        <div key={hashtag._id}>
-          <h2 className="text-blue-500">{hashtag._id}</h2>
-          <h3>{hashtag.count} tweets</h3>
-        </div>
-      ))}
+    <div className="mt-4 ">
+      <h2 className="text-xl font-semibold mb-2">Popular Hashtags</h2>
+      <div className="space-y-2">
+        {hashtags.map((hashtag) => (
+          <div className="w-[230px]" key={hashtag._id}>
+            <Link href={`/hashtag/${hashtag._id.substring(1)}`}>
+              <h3
+                style={{ overflowWrap: "anywhere" }}
+                className="text-blue-500 "
+              >
+                {hashtag._id}
+              </h3>
+            </Link>
+            <h4 className="text-sm text-gray-100">{hashtag.count} tweets</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
