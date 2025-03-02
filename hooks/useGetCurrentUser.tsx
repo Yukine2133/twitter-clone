b0264ca20db6fe878e9d131b03e3c0c7c7f6e011
Yukine213 <kondratenkov1109@gmail.com>
2025-03-02 19:35:23 +0200

    fix the typescript error during build

--- hooks/useGetCurrentUser.tsx
+++ hooks/useGetCurrentUser.tsx
@@ -4,6 +4,6 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 export const useGetCurrentUser = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-  const currentUser = await fetchUser(user?.id);
+  const currentUser = await fetchUser(user?.id as string);
   return { currentUser, user };
 };
