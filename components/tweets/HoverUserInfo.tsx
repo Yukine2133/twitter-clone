370a086178c26ab5836434d8f2a3b552b24b9d13
Yukine213 <kondratenkov1109@gmail.com>
2025-03-04 18:48:10 +0200

    refactor: change how the HoverUserInfo appears

--- components/tweets/HoverUserInfo.tsx
+++ components/tweets/HoverUserInfo.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import Link from "next/link";
 import { IUser } from "@/interfaces/user.interface";
 
@@ -11,57 +11,46 @@ interface IHoverUserInfoProps {
 }
 
 const HoverUserInfo = ({ children, user }: IHoverUserInfoProps) => {
-  const [show, setShow] = useState(false);
-
   return (
-    <div
-      className="relative flex flex-col items-center"
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
-    >
+    <div className="relative group flex flex-col items-center">
       {children}
-      {show && (
-        <div className="absolute top-4 left-[15px] z-20 mt-2 flex flex-col items-center transition-opacity duration-300 opacity-100 ">
-          <div className="relative z-20 w-64 overflow-hidden  text-white whitespace-no-wrap  bg-black shadow-sm shadow-white/80 rounded-lg ">
-            {user.backgroundImage ? (
+      <div className="absolute top-4 left-[15px] z-20 mt-2 flex flex-col items-center transition-opacity duration-300 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto">
+        <div className="relative z-20 w-64 overflow-hidden text-white whitespace-no-wrap bg-black shadow-sm shadow-white/80 rounded-lg">
+          {user.backgroundImage ? (
+            <Image
+              src={user.backgroundImage}
+              alt="Background image"
+              width={300}
+              height={20}
+              className="w-full rounded-b-md h-24 object-cover"
+            />
+          ) : (
+            <div className="w-full h-20 rounded-b-md bg-[#333639] object-cover" />
+          )}
+          <div className="flex items-center justify-between px-4 pt-2">
+            <Link href={`/profile/${user.username}`}>
               <Image
-                src={user.backgroundImage}
-                alt="Background image"
-                width={300}
-                height={20}
-                className="w-full rounded-b-md   h-24 object-cover"
+                src={user.avatar}
+                alt="Owner avatar"
+                width={62}
+                height={62}
+                className="rounded-full object-cover -translate-y-12"
               />
-            ) : (
-              <div className="w-full h-20 rounded-b-md  bg-[#333639] object-cover" />
-            )}
-            <div className="flex items-center justify-between px-4 pt-2 ">
-              <Link href={`/profile/${user.username}`}>
-                <Image
-                  src={user.avatar}
-                  alt="Owner avatar"
-                  width={62}
-                  height={62}
-                  className="rounded-full object-cover  -translate-y-12"
-                />
-              </Link>
-            </div>
-            <div className="w-fit px-4 -translate-y-4  ">
-              <Link
-                className="flex flex-col"
-                href={`/profile/${user.username}`}
-              >
-                <span className="font-semibold hover:underline w-[100px] truncate sm:w-fit">
-                  {user.displayName}
-                </span>
-                <span className="text-gray-500 text-[15px] w-[100px] truncate sm:w-fit ">
-                  @{user.username}
-                </span>
-              </Link>
-            </div>
-            <p className="mt-6 px-4 -translate-y-5 ">{user.bio}</p>
+            </Link>
           </div>
+          <div className="w-fit px-4 -translate-y-4">
+            <Link className="flex flex-col" href={`/profile/${user.username}`}>
+              <span className="font-semibold hover:underline w-[100px] truncate sm:w-fit">
+                {user.displayName}
+              </span>
+              <span className="text-gray-500 text-[15px] w-[100px] truncate sm:w-fit">
+                @{user.username}
+              </span>
+            </Link>
+          </div>
+          <p className="mt-6 px-4 -translate-y-5">{user.bio}</p>
         </div>
-      )}
+      </div>
     </div>
   );
 };
