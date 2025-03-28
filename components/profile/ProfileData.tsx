4e2994e892cdfa4ab771b9473715dc752adc0f46
Yukine213 <kondratenkov1109@gmail.com>
2025-03-28 15:13:24 +0200

    remove unnecessary imports

--- components/profile/ProfileData.tsx
+++ components/profile/ProfileData.tsx
@@ -1,23 +1,7 @@
-import Image from "next/image";
-import TweetCard from "@/components/tweets/TweetCard";
-import FollowButton from "@/components/buttons/FollowButton";
 import Follow from "@/components/follow/Follow";
-import type { ITweet } from "@/interfaces/tweet.interface";
-import {
-  CalendarDaysIcon,
-  EnvelopeIcon,
-  LockClosedIcon,
-  MapPinIcon,
-} from "@heroicons/react/24/outline";
-import { formatJoinedDate } from "@/utils/formatTimestamp";
 import type { ProfileDataProps } from "@/interfaces/user.interface";
-import Link from "next/link";
 import { ProfileHeader } from "./ProfileHeader";
-import { VerifiedBadge } from "../badges/VerifiedBadge";
-import ClientOnly from "../loaders/ClientOnly";
-import AdminBadge from "../badges/AdminBadge";
 import BannedUserMessage from "./BannedUserMessage";
-import { MoreButtonProfile } from "./moreButton/MoreButtonProfile";
 import { ProfileBackground } from "./ProfileBackground";
 import { ProfileActions } from "./ProfileActions";
 import { ProfileInfo } from "./ProfileInfo";
