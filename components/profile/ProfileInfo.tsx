0f0561e345db49d4cdd875184e55d00cfb201914
Yukine213 <kondratenkov1109@gmail.com>
2025-03-28 14:41:01 +0200

    refactor: change icons to lucide react and don't display verified badge if the user is admin

--- components/profile/ProfileInfo.tsx
+++ components/profile/ProfileInfo.tsx
@@ -1,8 +1,8 @@
 import { IUser } from "@/interfaces/user.interface";
 import AdminBadge from "../badges/AdminBadge";
 import { VerifiedBadge } from "../badges/VerifiedBadge";
-import { CalendarDaysIcon, MapPinIcon } from "@heroicons/react/24/outline";
 import { formatJoinedDate } from "@/utils/formatTimestamp";
+import { CalendarDays, MapPin } from "lucide-react";
 
 export const ProfileInfo = ({
   user,
@@ -18,11 +18,13 @@ export const ProfileInfo = ({
           <div className="flex items-center gap-1">
             <h2 className="font-bold text-xl mb-1">{user.displayName}</h2>
             {user.isAdmin && <AdminBadge />}
-            <VerifiedBadge
-              isOwner={isOwner}
-              isSubscribed={user.isSubscribed}
-              profileLink
-            />
+            {!user.isAdmin && (
+              <VerifiedBadge
+                isOwner={isOwner}
+                isSubscribed={user.isSubscribed}
+                profileLink
+              />
+            )}
           </div>
           <h2 className="text-neutral-500">@{user.username}</h2>
         </div>
@@ -32,12 +34,12 @@ export const ProfileInfo = ({
         <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-4">
           {user.location && (
             <div className="flex items-center gap-1 text-neutral-500">
-              <MapPinIcon className="h-5 w-5" />
+              <MapPin className="h-5 w-5" />
               <h3>{user.location}</h3>
             </div>
           )}
           <div className="text-neutral-500 flex items-center gap-2">
-            <CalendarDaysIcon className="h-5 w-5" />
+            <CalendarDays className="h-5 w-5" />
             <h3>{formatJoinedDate(user.createdAt)}</h3>
           </div>
         </div>
